b60c625385e611f97718acd69e0f12c468e14073
yousef <yousef01121977646@gmail.com>
2023-08-10 07:58:42 -0700

    j

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  print a coute  using  write function 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char quo[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write (1,quo,59);
+	return (1);
+}
