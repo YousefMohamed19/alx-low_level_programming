a97a224ded7fdfb7b2a834a17abcbe054e005d84
yousef <yousef01121977646@gmail.com>
2023-08-21 10:30:13 -0700

    j

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,9 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <main.h> 
 
-int _putchar(char c);
+int _putchar(chiar c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
